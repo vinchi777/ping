9c613e04a910c7d6d67cb81e4b22fe2c7b3ba672
vinchi777 <jude_calimbas@yahoo.com>
2018-02-03 02:22:15 +0800

    working

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from config import app_config
-
 import logging
 
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -17,14 +15,64 @@ def create_app(config_name):
 
     migrate = Migrate(app, db)
     from app import models
+    from app.models import Devise, TimeStamp
 
-
-    @app.route('/<devise_id>/<epoch_time>', methods=['POST'])
+    @app.route('/<devise_id>/<int:epoch_time>', methods=['POST'])
     def create(devise_id, epoch_time):
-        app.logger.info(devise_id)
-        app.logger.info(epoch_time)
-        app.logger.info(models.Ping)
-        return 'Hello World'
+        devise = Devise.save_ping(devise_id, epoch_time)
+        response = jsonify({
+            'id': devise.request_devise_id
+            })
+
+        response.status_code = 201
+        return response
+
+    @app.route('/<devise_id>/<from_date>', methods=['GET'])
+    @app.route('/<devise_id>/<from_date>/<to_date>', methods=['GET'])
+    def index(devise_id, from_date=None, to_date=None):
+        results = []
+
+        pings = TimeStamp.get_all(devise_id, from_date, to_date)
+        app.logger.info(pings)
+
+        if devise_id == 'all':
+            obj = {}
+            for ping in pings:
+                obj[str(ping.devise.request_devise_id)] = obj.get(str(ping.devise.request_devise_id), [])
+                obj[str(ping.devise.request_devise_id)].append(ping.ping_at.strftime('%s'))
+            results = obj
+        else:
+            for ping in pings:
+                obj = ping.ping_at.strftime('%s')
+                results.append(obj)
+
+        response = jsonify(results)
+        response.status_code = 201
+
+        return response
+
+    @app.route('/devices', methods=['GET'])
+    def devices():
+        results = []
+        devices = Devise.query.all()
+        for device in devices:
+            results.append(device.request_devise_id)
+
+        response = jsonify(results)
+        response.status_code = 201
+
+        return response
+
+    @app.route('/clear_data', methods=['POST'])
+    def clear_data():
+        db.session.query(TimeStamp).delete()
+        db.session.query(Devise).delete()
+        db.session.commit()
+
+        response = jsonify({})
+        response.status_code = 200
+
+        return response
 
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from app import db
+from datetime import datetime, date
+from sqlalchemy import cast, DATE
+import re
 
 class Devise(db.Model):
 
@@ -7,6 +10,20 @@ class Devise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     request_devise_id = db.Column(db.String(128), index=True, unique=True)
     time_stamps = db.relationship('TimeStamp', backref='devise', lazy='dynamic')
+
+    @staticmethod
+    def save_ping(devise_id, epoch_time):
+        formated_time = datetime.utcfromtimestamp(epoch_time)
+        devise = Devise.query.filter_by(request_devise_id=devise_id).first()
+
+        if devise is None: devise = Devise(request_devise_id=devise_id)
+
+        timestamp = TimeStamp(ping_at=formated_time)
+        devise.time_stamps.append(timestamp)
+        db.session.add(devise)
+        db.session.commit()
+        return devise
+
 
     def __repr__(self):
         return '<Devise: {}>'.format(self.request_devise_id)
@@ -18,6 +35,56 @@ class TimeStamp(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     devise_id = db.Column(db.Integer, db.ForeignKey('devises.id'))
+    ping_at = db.Column(db.DateTime(timezone=True))
+
+    @staticmethod
+    def get_all(devise_id, from_date, to_date):
+        result = None
+
+        if devise_id != 'all':
+            devise = Devise.query.filter_by(request_devise_id=devise_id).first()
+            if devise is None: return []
+            query = TimeStamp.single_or_range_query(from_date, to_date)
+            result = query.filter(TimeStamp.devise_id == devise.id).all()
+        else:
+            query = TimeStamp.single_or_range_query(from_date, to_date)
+            result = query.all()
+
+        return result
+
+
+    @staticmethod
+    def convert_to_datetime(date_str):
+        if re.match('^\d{4}-\d{2}-\d{2}$', date_str):
+            result = datetime.strptime(date_str, '%Y-%m-%d')
+        else:
+            # result = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(date_str)))
+            result = datetime.utcfromtimestamp(float(date_str))
+
+        return result
+
+    @staticmethod
+    def date_range_query(start_date, end_date):
+        query = TimeStamp.query \
+                .filter(TimeStamp.ping_at >= start_date) \
+                .filter(TimeStamp.ping_at < end_date)
+        return query
+
+    @staticmethod
+    def single_date_query(start_date):
+        query = TimeStamp.query.filter(cast(TimeStamp.ping_at, DATE) == start_date.date())
+        return query
+
+    @staticmethod
+    def single_or_range_query(from_date, to_date):
+        if to_date is None:
+            start_date = datetime.strptime(from_date, '%Y-%m-%d')
+            query = TimeStamp.single_date_query(start_date)
+        else:
+            start_date = TimeStamp.convert_to_datetime(from_date)
+            end_date = TimeStamp.convert_to_datetime(to_date)
+            query = TimeStamp.date_range_query(start_date, end_date)
+        return query
 
     def __repr__(self):
         return '<TimeStamp: {}>'.format(self.id)
